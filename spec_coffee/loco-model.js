1c23952b84894802d07a783fb85e9fa4fa59a6af
himn1 <zbigniew.humeniuk@gmail.com>
2017-10-31 10:22:24 +0100

    loco-model was updated

--- spec_coffee/loco-model.js
+++ spec_coffee/loco-model.js
@@ -1272,27 +1272,6 @@ en = {
   variants: {},
   models: {},
   attributes: {},
-  ui: {
-    form: {
-      sending: "Sending...",
-      success: "Success",
-      errors: {
-        connection: "Connection Error",
-        invalid_data: "Invalid data"
-      }
-    }
-  },
-  date: {
-    formats: {
-      "default": "%Y-%m-%d",
-      short: "%b %d",
-      long: "%B %d, %Y"
-    },
-    day_names: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-    abbr_day_names: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    month_names: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    abbr_month_names: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-  },
   errors: {
     messages: {
       accepted: "must be accepted",
