bd132ddf5a7412052291f0a091c843d32e1d9f0f
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-11-10 13:54:12 +0100

    Line, Wire and Utils are not exported anymore (unit tests fail only)

--- src/index.js
+++ src/index.js
@@ -2,28 +2,22 @@ import Channels from "./channels";
 import Controllers from "./controllers";
 import Env from "./env";
 import I18n from "./i18n";
-import Line from "./base/line.coffee";
 import Loco from "./base/loco.coffee";
 import Services from "./services";
-import Utils from "./utils";
 import Views from "./views";
-import Wire from "./base/wire.coffee";
 import { Models, Validators } from "./deps";
 
 const connector = { Env, I18n };
 
 export {
+  connector,
   Channels,
   Controllers,
   Env,
   I18n,
-  Line,
   Loco,
   Models,
   Services,
-  Utils,
   Validators,
-  Views,
-  Wire,
-  connector
+  Views
 };
