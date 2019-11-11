c112fc8a440a0a3f4463833c3d031d55ea3ac6ed
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-11-11 22:14:01 +0100

    test +1, spec -1

---
+++ __tests__/base/wire.test.js
@@ -0,0 +1,22 @@
+import Loco from "base/loco.coffee";
+import Wire from "base/wire.coffee";
+
+test('setting token correctly', () => {
+  const wire = new Wire;
+  wire.setToken('123qweasdzxc');
+  expect(wire._requestParams()).toEqual({ synced_at: null, token: '123qweasdzxc' });
+});
+
+describe('#setPollingTime', () => {
+  it('can change polling time', () => {
+    const loco = new Loco({ notifications: { enable: true } });
+    loco.init();
+    const wire = loco.getWire();
+
+    expect(wire.getPollingTime()).toEqual(3000);
+    const pollingInterval = wire.getPollingInterval();
+    wire.setPollingTime(10000);
+    expect(wire.getPollingTime()).toEqual(10000);
+    expect(wire.getPollingInterval()).not.toEqual(pollingInterval);
+  });
+});
