da7ad730266dd87d61083a8309c3c83db2f2ca4a
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-11-12 22:28:00 +0100

    +2 tests, -2 specs

--- __tests__/base/wire.test.js
+++ __tests__/base/wire.test.js
@@ -8,14 +8,17 @@ class Article extends Models.Base {
 
 Object.assign(Models, { Article });
 
-test('setting token correctly', () => {
-  const wire = new Wire;
-  wire.setToken('123qweasdzxc');
-  expect(wire._requestParams()).toEqual({ synced_at: null, token: '123qweasdzxc' });
+test("setting token correctly", () => {
+  const wire = new Wire();
+  wire.setToken("123qweasdzxc");
+  expect(wire._requestParams()).toEqual({
+    synced_at: null,
+    token: "123qweasdzxc"
+  });
 });
 
-describe('#setPollingTime', () => {
-  it('can change polling time', () => {
+describe("#setPollingTime", () => {
+  it("can change polling time", () => {
     const wire = getWire();
     expect(wire.getPollingTime()).toEqual(3000);
     const pollingInterval = wire.getPollingInterval();
@@ -26,10 +29,31 @@ describe('#setPollingTime', () => {
 });
 
 describe("#processNotification", () => {
-  it('returns if imap is empty', () => {
+  it("returns if imap is empty", () => {
     const wire = getWire();
-    const result = wire.processNotification(["Article", 1, "created", {id: 1}]);
+    const result = wire.processNotification([
+      "Article",
+      1,
+      "created",
+      { id: 1 }
+    ]);
     expect(result).toBe(undefined);
+  });
+});
+
+describe("#_getURL", () => {
+  it("returns url with choosen protocol", () => {
+    const wire = getWire();
+    wire.setSSL(true);
+    const [protocol] = wire._getURL().split("/");
+    expect(protocol).toEqual("https:");
+  });
+
+  it("return correct notifications's location", () => {
+    const wire = getWire();
+    wire.setLocation("nc");
+    const [, , , location] = wire._getURL().split("/");
+    expect(location).toEqual("nc");
   });
 });
 
@@ -37,4 +61,4 @@ const getWire = () => {
   const loco = new Loco({ notifications: { enable: true } });
   loco.init();
   return loco.getWire();
-}
+};
--- spec_coffee/loco/base/wire_spec.coffee
+++ spec_coffee/loco/base/wire_spec.coffee
@@ -5,17 +5,6 @@ describe "App.Wire", ->
   afterEach ->
     @wire.disconnect()
 
-  describe "#_getURL", ->
-    it "returns url with choosen protocol", ->
-      @wire.setSSL true
-      [protocol] = @wire._getURL().split '/'
-      expect(protocol).toEqual 'https:'
-
-    it "return correct notifications's location", ->
-      @wire.setLocation 'nc'
-      [_, _, _, location] = @wire._getURL().split '/'
-      expect(location).toEqual 'nc'
-
   describe "#setSize", ->
     it "can set max size of notifications", ->
       @wire.setSize 10
