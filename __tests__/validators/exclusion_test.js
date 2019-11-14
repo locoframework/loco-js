d15495607368702666b357b35cf2c6f0d92c0844
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-11-14 10:14:44 +0100

    +1 test, -1 spec

--- __tests__/validators/exclusion_test.js
+++ __tests__/validators/exclusion_test.js
@@ -13,6 +13,21 @@ class Dummy extends Models.Base {
   };
 }
 
+class DummyCustomMsg extends Models.Base {
+  static identity = "DummyCustomMsg";
+
+  static attributes = {
+    author: {
+      validations: {
+        exclusion: {
+          in: ["admin", "superadmin"],
+          message: "being an admin is not for u"
+        }
+      }
+    }
+  };
+}
+
 I18n.pl = {
   errors: {
     messages: {
@@ -40,4 +55,10 @@ it("supports i18n", () => {
   const dummy = new Dummy({ author: "admin" });
   dummy.isValid();
   expect(dummy.errors.author[0]).toEqual("jest zarezerwowane");
+});
+
+it("supports custom message", () => {
+  const dcm = new DummyCustomMsg({ author: "admin" });
+  dcm.isValid();
+  expect(dcm.errors.author[0]).toEqual("being an admin is not for u");
 });
