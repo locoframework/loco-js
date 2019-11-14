8c936c517d5e9d1ca046e42e4b1055a308b8856e
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-11-14 21:25:22 +0100

    +1 test, -1 spec

--- __tests__/validators/inclusion_test.js
+++ __tests__/validators/inclusion_test.js
@@ -1,4 +1,5 @@
-import { Models } from "index";
+import Loco from "base/loco.coffee";
+import { I18n, Models } from "index";
 
 class Dummy extends Models.Base {
   static identity = "Dummy";
@@ -12,6 +13,14 @@ class Dummy extends Models.Base {
   };
 }
 
+I18n.pl = {
+  errors: {
+    messages: {
+      inclusion: "nie znajduje się na liście dopuszczalnych wartości"
+    }
+  }
+};
+
 it("adds error if value is not available in an array", () => {
   const dummy = new Dummy({ rate: "decent" });
   dummy.isValid();
@@ -22,4 +31,14 @@ it("is valid if value is available in an array", () => {
   const dummy = new Dummy({ rate: "good" });
   dummy.isValid();
   expect(dummy.errors).toBe(null);
+});
+
+it("supports i18n", () => {
+  const loco = new Loco();
+  loco.setLocale("pl");
+  const dummy = new Dummy({ rate: "decent" });
+  dummy.isValid();
+  expect(dummy.errors.rate[0]).toEqual(
+    "nie znajduje się na liście dopuszczalnych wartości"
+  );
 });
--- spec_coffee/loco/validators/inclusion_spec.coffee
+++ spec_coffee/loco/validators/inclusion_spec.coffee
@@ -4,11 +4,6 @@ describe 'App.Validators.Inclusion', ->
 
   afterEach -> App.Env.loco.setLocale 'en'
 
-  it 'supports i18n', ->
-    App.Env.loco.setLocale 'pl'
-    @dummy.isValid()
-    expect(@dummy.errors.rate[0]).toEqual "nie znajduje się na liście dopuszczalnych wartości"
-
   it 'supports custom message', ->
     dcm = new App.Models.DummyCustomMsg rate: 'decent'
     dcm.isValid()
