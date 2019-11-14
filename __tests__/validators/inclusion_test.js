024785d735803e42ad1d0e9e8542d825802422bb
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-11-14 21:28:48 +0100

    +1 test, -1 spec

--- __tests__/validators/inclusion_test.js
+++ __tests__/validators/inclusion_test.js
@@ -13,6 +13,21 @@ class Dummy extends Models.Base {
   };
 }
 
+class DummyCustomMsg extends Models.Base {
+  static identity = "DummyCustomMsg";
+
+  static attributes = {
+    rate: {
+      validations: {
+        inclusion: {
+          within: ["bad", "good", "excellent"],
+          message: "value is not good"
+        }
+      }
+    }
+  };
+}
+
 I18n.pl = {
   errors: {
     messages: {
@@ -41,4 +56,10 @@ it("supports i18n", () => {
   expect(dummy.errors.rate[0]).toEqual(
     "nie znajduje się na liście dopuszczalnych wartości"
   );
+});
+
+it("supports custom message", () => {
+  const dcm = new DummyCustomMsg({ rate: "decent" });
+  dcm.isValid();
+  expect(dcm.errors.rate[0]).toEqual("value is not good");
 });
