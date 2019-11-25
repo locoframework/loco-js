6d207c5557c4a4fcbb988fff59f03738cb2d9ae6
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-11-26 00:06:24 +0100

    +3 specs, -3 tests

---
+++ __tests__/validators/presence_test.js
@@ -0,0 +1,63 @@
+import Loco from "base/loco.coffee";
+import { I18n, Models } from "index";
+
+class Article extends Models.Base {
+  static identity = "Article";
+
+  static attributes = {
+    title: {
+      validations: {
+        presence: true
+      }
+    }
+  };
+}
+
+class DummyCustomMsg extends Models.Base {
+  static identity = "Dummy";
+
+  static attributes = {
+    title: {
+      validations: {
+        presence: { message: "presence is required" }
+      }
+    }
+  };
+}
+
+I18n.pl = {
+  errors: {
+    messages: {
+      blank: "nie może być puste"
+    }
+  }
+};
+
+const loco = new Loco();
+let article = null;
+
+beforeEach(() => {
+  article = new Article({ title: "" });
+});
+
+afterEach(() => {
+  loco.setLocale("en");
+  article = new Article({ title: "" });
+});
+
+it("validates length if object is a string", () => {
+  article.isValid();
+  expect(article.errors.title[0]).toEqual("can't be blank");
+});
+
+it("supports i18n", () => {
+  loco.setLocale("pl");
+  article.isValid();
+  expect(article.errors.title[0]).toEqual("nie może być puste");
+});
+
+it("supports custom message", () => {
+  const dcm = new DummyCustomMsg({ title: null });
+  dcm.isValid();
+  expect(dcm.errors.title[0]).toEqual("presence is required");
+});
