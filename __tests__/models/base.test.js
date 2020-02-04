8629c6185eec906d35094bc75d2a29391e12d57d
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-02-04 23:06:27 +0100

    +2 tests, -2 specs

--- __tests__/models/base.test.js
+++ __tests__/models/base.test.js
@@ -3,7 +3,6 @@ import { Models } from "index";
 
 class Comment extends Models.Base {
   static identity = "Article.Comment";
-  //static remoteName = "Comment";
   static resources = {
     url: "/user/articles/:articleId/comments",
     paginate: { per: 10 }
@@ -22,6 +21,32 @@ class Article extends Models.Base {
     },
     adminReviewStartedAt: {
       type: "Number"
+    }
+  };
+
+  static validate = ["vulgarityLevel"];
+
+  vulgarityLevel = () => {
+    if (this.content != null && /fuck/i.exec(this.content)) {
+      this.addErrorMessage("Article contains strong language.", {
+        for: "base"
+      });
+    }
+  };
+}
+
+class Dummy extends Models.Base {
+  static identity = "Dummy";
+
+  static attributes = {
+    dumbAttrib5: {
+      validations: {
+        size: { minimum: 1 },
+        format: {
+          with: /^[a-z0-9]{5,}$/,
+          if: o => o.dumbAttrib5 != null && o.dumbAttrib5.length >= 5
+        }
+      }
     }
   };
 }
@@ -60,5 +85,22 @@ describe("attribute types", () => {
   it("can be String", () => {
     const article = new Article({ title: 12.33 });
     expect(article.title).toEqual("12.33");
+  });
+});
+
+describe("validation", () => {
+  it("allows custom validations", () => {
+    const article = new Article({ content: "Some words.. and fUCk!" });
+    expect(article.isInvalid()).toBe(true);
+    expect(article.errors.base[0]).toEqual("Article contains strong language.");
+  });
+
+  it("supports conditional validation", () => {
+    const dummy = new Dummy({ dumbAttrib5: "KRAKOW" });
+    dummy.isValid();
+    expect(dummy.errors.dumbAttrib5[0]).toEqual("is invalid");
+    dummy.dumbAttrib5 = "KRK";
+    dummy.isValid();
+    expect(dummy.errors).toBe(null);
   });
 });
--- spec_coffee/loco/models/base_spec.coffee
+++ spec_coffee/loco/models/base_spec.coffee
@@ -1,18 +1,4 @@
 describe 'App.Models.Base', ->
-  describe 'validation', ->
-    it 'allows custom validations', ->
-      article = new App.Models.Article content: 'Some words.. and fUCk!'
-      expect(article.isInvalid()).toBe true
-      expect(article.errors.base[0]).toEqual 'Article contains strong language.'
-
-    it 'supports conditional validation', ->
-      dummy = new App.Models.Dummy dumbAttrib5: 'KRAKOW'
-      dummy.isValid()
-      expect(dummy.errors.dumbAttrib5[0]).toEqual 'is invalid'
-      dummy.dumbAttrib5 = 'KRK'
-      dummy.isValid()
-      expect(dummy.errors.dumbAttrib5).toBe undefined
-
   describe '#save', ->
     it 'properly builds URL for nested models', ->
       comment = new App.Models.Article.Comment articleId: 1, author: 'Joe Doe', text: 'foo bar baz'
