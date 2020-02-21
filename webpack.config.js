63805bf3728db8ba3e9fa8416f893702f2d3a970
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-02-21 23:57:08 +0100

    [feature] Change lib's name to Loco (output -> library)

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "loco.js",
-    library: "App",
+    library: "Loco",
     libraryTarget: "umd"
   },
   externals: {
