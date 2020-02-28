c71fc2dea29efb1d9b914650ab6e55702d7f28db
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-02-29 00:32:02 +0100

    [feature] remove unneeded locale

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import Channels from "./channels";
 import Env from "./env";
-import I18n from "./i18n";
 import Loco from "./base/loco.coffee";
-import { Controllers, Models, Validators, Views } from "./deps";
+import { Controllers, I18n, Models, Validators, Views } from "./deps";
 
 const init = opts => new Loco(opts).init();
 const connector = { Env, I18n };
