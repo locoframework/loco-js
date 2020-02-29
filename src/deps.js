87ce423bd5cb9e81487475b016a9e461f5f26e71
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-02-29 11:25:57 +0100

    [feature] no Object.assign

--- src/deps.js
+++ src/deps.js
@@ -1,24 +1,32 @@
 import { init as initCore, Controllers, Views } from "loco-js-core";
 
-import {
-  Config,
-  I18n,
-  IdentityMap,
-  Models,
-  Validators
-} from "loco-js-model";
+import { Config, I18n, IdentityMap, Models, Validators } from "loco-js-model";
 
-import Connectivity from './base/mixins/Connectivity'
+import Connectivity from "./base/mixins/Connectivity";
 
-Controllers.Base.prototype.setScope = name => Config.scope = name;
+Controllers.Base.prototype.setScope = name => (Config.scope = name);
 Controllers.Base.prototype.setResource = name => this.setScope(name);
 
-Object.assign(Controllers.Base.prototype, Connectivity);
-Object.assign(Views.Base.prototype, Connectivity);
+for (const property in Connectivity) {
+  if (Object.prototype.hasOwnProperty.call(Connectivity, property)) {
+    Controllers.Base.prototype[property] = Connectivity[property];
+    Views.Base.prototype[property] = Connectivity[property];
+  }
+}
 
 const Deps = {
   cable: null,
   NotificationCenter: null
 };
 
-export { initCore, Config, Controllers, Deps, I18n, IdentityMap, Models, Validators, Views };
+export {
+  initCore,
+  Config,
+  Controllers,
+  Deps,
+  I18n,
+  IdentityMap,
+  Models,
+  Validators,
+  Views
+};
