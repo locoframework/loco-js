6c8e3af8f45f6d5f72217e31257ddeb330a2a51d
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-03-16 22:25:12 +0100

    [feature] no Connectivity

--- src/deps.js
+++ src/deps.js
@@ -2,17 +2,8 @@ import { init as initCore, Controllers, Views } from "loco-js-core";
 
 import { Config, I18n, IdentityMap, Models, Validators } from "loco-js-model";
 
-import Connectivity from "./base/mixins/Connectivity";
-
 Controllers.Base.prototype.setScope = name => (Config.scope = name);
 Controllers.Base.prototype.setResource = name => this.setScope(name);
-
-for (const property in Connectivity) {
-  if (Object.prototype.hasOwnProperty.call(Connectivity, property)) {
-    Controllers.Base.prototype[property] = Connectivity[property];
-    Views.Base.prototype[property] = Connectivity[property];
-  }
-}
 
 const Deps = {
   cable: null,
