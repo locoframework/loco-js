c8511c82988179064e33194b65b9ea414728b153
himn1 <hello@artofcode.co>
2022-01-19 23:49:27 +0100

    [test]

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -33,6 +33,14 @@ describe("init", () => {
     expect(Models.Article.protocolWithHost).toEqual("https://localhost:3003/");
   });
 
+  it("sets authorizationHeader on models if passed as a config", () => {
+    init({
+      models: { Article: { foo: "bar" } },
+      authorizationHeader: "Bearer XXX",
+    });
+    expect(Models.Article.authorizationHeader).toEqual("Bearer XXX");
+  });
+
   it("sets controllers", () => {
     init({ controllers: { ArticleController: { foo: "bar" } } });
     expect(Controllers.ArticleController).toEqual({ foo: "bar" });
